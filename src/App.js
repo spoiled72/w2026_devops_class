f328b860ec0a6c5fac419129147275cb97d16345
spoiled72 <spoiledboy72@gmail.com>
2026-02-18 14:50:20 -0800

    updated file and student name and id

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Studen ID: Put Your Student ID</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
+      <h1>Studen ID: 101589060</h1>
+      <h1>Studen ID: Richard Miller</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
